55cceecd48c4bec9981b2a179c01d85329f3782e
Eric Litwin <eric.litwin@pnmac.com>
2016-03-07 12:39:44 -0800

    implementing Enum and List functions without library functions or list comprehensions

--- my_list.exs
+++ my_list.exs
@@ -44,4 +44,61 @@ defmodule MyList do
   # def span(from, to), do: Enum.to_list from..to[
   def span(from, to) when from > to, do: []
   def span(from, to), do: [ from | span(from + 1, to) ]
+
+  # all?
+  def all?(list),      do: all?(list, fn x -> !!x end)
+  def all?([], _func), do: true
+  def all?([ head | tail ], func) do
+    if func.(head) do
+      all?(tail, func)
+    else
+      false
+    end
+  end
+
+  # each
+  def each([], _func), do: []
+  def each([ head | tail ], func) do
+    [ func.(head) | each(tail, func) ]
+  end
+
+  # filter
+  def filter([], _func), do: []
+  def filter([ head | tail ], func) do
+    if func.(head) do
+      [ head | filter(tail, func) ]
+    else
+      filter(tail, func)
+    end
+  end
+
+  # split
+  def split([head | tail], count) when count > 0 do
+    {left, right} = split(tail, count-1)
+    {[head | left], right}
+  end
+  def split(list, _count), do: {[], list}
+
+  # take (with library function)
+  # def take(list, n), do: hd(Tuple.to_list(split(list, n)))
+  # take without library function
+  def take( [ head | tail ], n) when n > 0 do
+    [ head | take(tail, n - 1) ]
+  end
+  def take(_list, _count), do: []
+
+  # flatten
+  def flatten(list), do: _flatten(list, [])
+
+  defp _flatten([], tail), do: tail
+  defp _flatten([ h | t ], tail) when is_list(h) do
+    _flatten(h, _flatten(t, tail))
+  end
+  defp _flatten([ h | t], tail) do
+    [h | _flatten(t, tail)]
+  end
+
+  def flatten2([]), do: []
+  def flatten2([h|t]) when is_list(h), do: flatten2(h) ++ flatten2(t)
+  def flatten2([h|t]), do: [h] ++ flatten2(t)
 end
